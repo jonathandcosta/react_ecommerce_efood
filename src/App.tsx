61fee68ab4c7a20239e1eca8a82dd20c8410e722
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-01-16 21:23:58 -0300

    config redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
+import { Provider } from "react-redux"
+
 import { BrowserRouter } from "react-router-dom"
 
 import { GlobalCss } from "./styles"
 import Rotas from './routes'
 import Footer from "./components/Footer"
+import { store } from "./store"
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <div>
-        <Rotas />
-      </div>
-      <Footer />
-    </BrowserRouter>
+
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalCss />
+        <div>
+          <Rotas />
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
