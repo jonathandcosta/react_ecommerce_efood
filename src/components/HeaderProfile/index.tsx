6e3523e7907db2a3d73f6d070773b879f11c3628
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-02-10 19:19:12 -0300

    duvida para interação com o carrinho

--- src/components/HeaderProfile/index.tsx
+++ src/components/HeaderProfile/index.tsx
@@ -4,16 +4,28 @@ import { ImagemHero, ImagemLogo, Name } from './styles'
 import img from '../../assets/img/hero1.png'
 import logo from '../../assets/icons/logo.png'
 
-const HeaderProfile = () => (
-  <>
-    <ImagemHero style={{ backgroundImage: `url(${img})` }}>
-      <Link to="/">
-        <Name>Restaurantes</Name>
-      </Link>
-      <ImagemLogo style={{ backgroundImage: `url(${logo})` }}></ImagemLogo>
-      <Name>0 Produto no Carrinho</Name>
-    </ImagemHero>
-  </>
-)
+import { open } from '../../store/reducers/cart'
+import { useDispatch } from 'react-redux'
+
+const HeaderProfile = () => {
+  const dispatch = useDispatch()
+  const openCart = () => {
+    dispatch(open())
+  }
+
+  return (
+    (
+      <>
+        <ImagemHero style={{ backgroundImage: `url(${img})` }}>
+          <Link to="/">
+            <Name>Restaurantes</Name>
+          </Link>
+          <ImagemLogo style={{ backgroundImage: `url(${logo})` }}></ImagemLogo>
+          <Link to={"#"} onClick={openCart}><Name>0 Produto no Carrinho</Name></Link>
+        </ImagemHero>
+      </>
+    )
+  )
+}
 
 export default HeaderProfile
