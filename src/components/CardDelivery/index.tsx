f68d6145d82884a72941f646850d2874a45e0f12
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-03-22 16:57:49 -0300

    aplicando validações com formik

--- src/components/CardDelivery/index.tsx
+++ src/components/CardDelivery/index.tsx
@@ -1,41 +1,99 @@
+import { useFormik } from 'formik'
 import { InputGroup, ContainerDelivery, NumberAdress, Buttons, ButtonDelivery } from './style'
 
-const CardDelivery = () => (
-  <ContainerDelivery>
-    <div>
-      <h2>Entrega</h2>
-      <InputGroup>
-        <label htmlFor="fullName">Quem irá receber</label>
-        <input id="fullName" type="text" />
-      </InputGroup>
-      <InputGroup>
-        <label htmlFor="adress">Endereço</label>
-        <input id="adress" type="text" />
-      </InputGroup>
-      <InputGroup>
-        <label htmlFor="city">Cidade</label>
-        <input id="city" type="text" />
-      </InputGroup>
-      <NumberAdress>
-        <InputGroup maxWidth="155px">
-          <label htmlFor="cep">CEP</label>
-          <input id="cep" type="text" />
+const CardDelivery = () => {
+
+  const form = useFormik({
+    initialValues: {
+      fullName: '',
+      adress: '',
+      city: '',
+      cep: '',
+      numberHome: '',
+      completAdress: '',
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+  console.log(form)
+
+  return (
+    <ContainerDelivery>
+      <form onSubmit={form.handleSubmit}>
+        <h2>Entrega</h2>
+        <InputGroup>
+          <label htmlFor="fullName">Quem irá receber</label>
+          <input
+            id="fullName"
+            type="text"
+            name='fullName'
+            value={form.values.fullName}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+          />
         </InputGroup>
-        <InputGroup maxWidth="155px">
-          <label htmlFor="numberHome">Número</label>
-          <input id="numberHome" type="text" />
+        <InputGroup>
+          <label htmlFor="adress">Endereço</label>
+          <input
+            id="adress"
+            type="text"
+            name='adress'
+            value={form.values.adress}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur} />
         </InputGroup>
-      </NumberAdress>
-      <InputGroup>
-        <label htmlFor="completAdress">Complemento (opcional)</label>
-        <input id="completAdress" type="text" />
-      </InputGroup>
-      <Buttons>
-        <ButtonDelivery type='button' title='clique aqui para ir ao pagamento'>Continuar com o pagamento</ButtonDelivery>
-        <ButtonDelivery type='button' title='clique aqui para voltar ao carrinho'>Voltar para o carrinho</ButtonDelivery>
-      </Buttons>
-    </div>
-  </ContainerDelivery >
-)
+        <InputGroup>
+          <label htmlFor="city">Cidade</label>
+          <input
+            id="city"
+            type="text"
+            name='city'
+            value={form.values.city}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur} />
+        </InputGroup>
+        <NumberAdress>
+          <InputGroup maxWidth="155px">
+            <label htmlFor="cep">CEP</label>
+            <input
+              id="cep"
+              type="text"
+              name='cep'
+              value={form.values.cep}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur} />
+          </InputGroup>
+          <InputGroup maxWidth="155px">
+            <label htmlFor="numberHome">Número</label>
+            <input
+              id="numberHome"
+              type="text"
+              name='numberHome'
+              value={form.values.numberHome}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+          </InputGroup>
+        </NumberAdress>
+        <InputGroup>
+          <label htmlFor="completAdress">Complemento (opcional)</label>
+          <input
+            id="completAdress"
+            type="text"
+            name='completAdress'
+            value={form.values.completAdress}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur} />
+        </InputGroup>
+        <Buttons>
+          <ButtonDelivery type='button' title='clique aqui para ir ao pagamento'>Continuar com o pagamento</ButtonDelivery>
+          <ButtonDelivery type='button' title='clique aqui para voltar ao carrinho'>Voltar para o carrinho</ButtonDelivery>
+        </Buttons>
+      </form>
+    </ContainerDelivery >
+  )
+}
 
 export default CardDelivery
--- src/components/CardPayment/index.tsx
+++ src/components/CardPayment/index.tsx
@@ -1,35 +1,84 @@
+import { useFormik } from 'formik'
 import { InputGroup, ContainerDelivery, Buttons, ButtonDelivery } from './style'
 
-const CardPayment = () => (
-  <ContainerDelivery>
-    <div>
-      <h2>Pagamento - Valor a pagar R$ 190,90</h2>
-      <InputGroup>
-        <label htmlFor="fullName">Nome no cartão</label>
-        <input type="text" id='fullName' />
-      </InputGroup>
-      <InputGroup>
-        <label htmlFor="numberCard">Número do cartão</label>
-        <input type="text" id='numberCard' />
-      </InputGroup>
-      <InputGroup>
-        <label htmlFor="cvv">CVV</label>
-        <input type="text" id='cvv' />
-      </InputGroup>
-      <InputGroup>
-        <label htmlFor="monthCard">Mês de vencimento</label>
-        <input type="text" id='monthCard' />
-      </InputGroup>
-      <InputGroup>
-        <label htmlFor="yearCard">Ano de vencimento</label>
-        <input type="text" id='yearCard' />
-      </InputGroup>
-      <Buttons>
-        <ButtonDelivery type='button' title='clique aqui para finalizar o pagamento'>Finalizar pagamento</ButtonDelivery>
-        <ButtonDelivery type='button' title='clique aqui para voltar editar o endereço'>Voltar para a edição de endereço</ButtonDelivery>
-      </Buttons>
-    </div>
-  </ContainerDelivery>
-)
+const CardPayment = () => {
+
+  const form = useFormik({
+    initialValues: {
+      fullName: '',
+      numberCard: '',
+      cvv: '',
+      monthCard: '',
+      yearCard: '',
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+  console.log(form)
+
+  return (
+    <ContainerDelivery>
+      <form onSubmit={form.handleSubmit}>
+        <h2>Pagamento - Valor a pagar R$ 190,90</h2>
+        <InputGroup>
+          <label htmlFor="fullName">Nome no cartão</label>
+          <input
+            type="text"
+            id='fullName'
+            name='fullName'
+            value={form.values.fullName}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur} />
+        </InputGroup>
+        <InputGroup>
+          <label htmlFor="numberCard">Número do cartão</label>
+          <input
+            type="text"
+            id='numberCard'
+            name='numberCard'
+            value={form.values.numberCard}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur} />
+        </InputGroup>
+        <InputGroup>
+          <label htmlFor="cvv">CVV</label>
+          <input
+            type="text"
+            id='cvv'
+            name='cvv'
+            value={form.values.cvv}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur} />
+        </InputGroup>
+        <InputGroup>
+          <label htmlFor="monthCard">Mês de vencimento</label>
+          <input
+            type="text"
+            id='monthCard'
+            name='monthCard'
+            value={form.values.monthCard}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur} />
+        </InputGroup>
+        <InputGroup>
+          <label htmlFor="yearCard">Ano de vencimento</label>
+          <input
+            type="text"
+            id='yearCard'
+            name='yearCard'
+            value={form.values.yearCard}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur} />
+        </InputGroup>
+        <Buttons>
+          <ButtonDelivery type='button' title='clique aqui para finalizar o pagamento'>Finalizar pagamento</ButtonDelivery>
+          <ButtonDelivery type='button' title='clique aqui para voltar editar o endereço'>Voltar para a edição de endereço</ButtonDelivery>
+        </Buttons>
+      </form>
+    </ContainerDelivery>
+  )
+}
 
 export default CardPayment
